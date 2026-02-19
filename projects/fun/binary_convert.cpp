1da291200589decfd490252c26e5bd729776a515
Aurora-Jennifer <jtlewis204@gmail.com>
2026-02-18 22:22:43 -0600

    something for discrete structures homework

--- projects/fun/binary_convert.cpp
+++ projects/fun/binary_convert.cpp
@@ -9,9 +9,12 @@ vector<int> convert(int number) {
   vector<int> binary;
 
   while (n > 0) {
-    remainder = n % 2;
+    remainder =
+        n %
+        2; // grabs the remainder and pushes it back to the vector for storage
     binary.push_back(remainder);
-    n = n / 2;
+    n = n / 2; // divides n by 2 to find the next number to find the remainer
+               // for
   }
 
   return binary;
@@ -24,7 +27,9 @@ int main() {
   cin >> number;
 
   vector<int> conversion = convert(number);
-  reverse(conversion.begin(), conversion.end());
+  reverse(conversion.begin(),
+          conversion
+              .end()); // reverses it becuase its top down, instead of bottom up
 
   for (int i = 0; i < (int)conversion.size(); i++) {
     cout << conversion[i];
