42754416bc6ec791b797066daafdbf93990bc115
Aurora-Jennifer <jtlewis204@gmail.com>
2026-02-18 22:28:07 -0600

    something for discrete structures homework

--- projects/fun/binary_convert.cpp
+++ projects/fun/binary_convert.cpp
@@ -32,7 +32,8 @@ int main() {
               .end()); // reverses it becuase its top down, instead of bottom up
 
   for (int i = 0; i < (int)conversion.size(); i++) {
-    if (i % 7 == 0) {
+    if (i % 7 == 0) { // mod 7 because 0 is actually 1, and every mod 7 r 0 is
+                      // the 8th place
       cout << " " << conversion[i];
     }
     cout << conversion[i];
