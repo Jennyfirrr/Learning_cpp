34a3ca4e059de282379259d3d520a5e07ce44a68
Aurora-Jennifer <jtlewis204@gmail.com>
2026-02-19 20:13:19 -0600

    got the optimal solution first try :)

---
+++ projects/leetcode/build_array_stack.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> buildArray(vector<int> &target, int n) {
+  vector<string> operations;
+  int j = 0; // counter to match the vector index and updating it when a match
+             // is found
+
+  for (int i = 0; i < n; i++) {
+    if (i + 1 == target[j]) {
+      operations.push_back("Push");
+      j++;
+    } else {
+      operations.push_back("Push"); // if i isnt a match with the associated
+                                    // index in target, then it pushes and pops,
+                                    // because thats what the problem said
+      operations.push_back("Pop");
+    }
+    if (j ==
+        target.size()) { // once j reaches the size of the target array, the
+                         // loop ends because it will have the ideal stack
+                         // operations to build the stack the problem wanted
+      break;
+    }
+  }
+  return operations;
+}
