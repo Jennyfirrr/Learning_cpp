1f316cff05149498d55a0187913686f6fedbfeba
Aurora-Jennifer <jtlewis204@gmail.com>
2026-02-20 17:29:09 -0600

    playing with bitshifting for division

--- projects/deep_dives/it_shift.cpp
+++ projects/deep_dives/it_shift.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int bitShift(vector<int> &nums, int shift) {
 
   cout << "Bits shifted right: " << shift << endl;
+  // every shift right is dividing by 2^n, where n is the shift right, so 1
+  // shift is x/2, 2 shift is x/4,etc
 
   for (int n : nums) {
     int result = n >> shift;
