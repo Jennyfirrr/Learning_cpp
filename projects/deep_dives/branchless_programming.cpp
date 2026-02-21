28499c6a57be2ce262c425f1a1a0ac7b7fbcdaa2
Aurora-Jennifer <jtlewis204@gmail.com>
2026-02-21 06:31:52 -0600

    placeholder while i eat breakfast

--- projects/deep_dives/branchless_programming.cpp
+++ projects/deep_dives/branchless_programming.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
 #include <vector>
 
-// were expanding on bitwise operators with branchless programming today
+// were expanding on bitwise operators with branchless programming today, i may
+// go back and make notes for all the operators, like &, |, ^, <<, >>, ?, and a
+// few others before I do this, idk, im seeing where the rabbit hole takes me
