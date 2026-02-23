1d6f23274818beb7fff641dafcf67027f8f03ad9
Aurora-Jennifer <jtlewis204@gmail.com>
2026-02-23 03:18:37 -0600

    some minor updates

--- projects/deep_dives/new_notes/chaining_check.cpp
+++ projects/deep_dives/new_notes/chaining_check.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 
+// ============================================================================
 // N O T I C E
+// ============================================================================
 //
 // these are gonna start assuming that your familiar with the bitwise operators
 // and stuff going forward, ill still probably do the binary bit by bit
@@ -10,6 +12,7 @@
 // but i hope they can help someone else too, the way i type these keeps me
 // engaged when i reread these lol, hopefully theyre as entertaining for you as
 // they are me
+// ============================================================================
 
 // WIP FOR ACTUALLY BUILDING ORDERBOOK LOGIC, i understand that its better to
 // use a single reusable function, but im doing it this way to actually see how
