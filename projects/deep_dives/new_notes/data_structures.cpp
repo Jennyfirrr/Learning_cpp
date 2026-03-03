11a09a1b44d5536637a613dd64e9cd98ba4ed0c9
Aurora-Jennifer <jtlewis204@gmail.com>
2026-03-03 00:30:37 -0600

    C H I L D I S H

--- projects/deep_dives/new_notes/data_structures.cpp
+++ projects/deep_dives/new_notes/data_structures.cpp
@@ -1,10 +1,27 @@
 //==============================================================================
 // [DATA STRUCTURES WOOHOO] [trees and shit and graphs and stuff] [java-bad]
 //==============================================================================
-// gonna go over data structures here
+// [LINEAR STRUCTURES]
+//==============================================================================
+// so these are basically things like arrays, vectors, linked lists
+//
+// arrays are typically stored in the stack, and generally have a faster access
+// time because its just a pointer and a reference pointer for the base, DWARF
+// unwinders are used to free up the %rbp register when using these
+//
+// vectors are stored in the heap, theyre basically contiguous blocks of memory,
+// slower to access than an array, and exeists outside of runtime for functions
+//
+// linked lists, these are just nodes pointing to other nodes, theyre great for
+// frequent insertions, but you cant just jump to the middle, you have to walk
+// there
+//==============================================================================
+// [TREES]
 //==============================================================================
 //==============================================================================
+// [GRAPHS]
 //==============================================================================
 //==============================================================================
+// [HASH MAPS]
 //==============================================================================
 //==============================================================================
